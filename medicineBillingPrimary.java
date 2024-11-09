23605da0cb976d0e38f364d1be61a35f98d5e49a
musfique-anam <musfique2002.personal@gmail.com>
2024-11-09 23:09:35 +0600

    Working

--- medicineBillingPrimary.java
+++ medicineBillingPrimary.java
@@ -40,9 +40,32 @@ public class medicineBillingPrimary extends JFrame {
     }
 
     private void setupUI(){
+        Jpanel inputPanel = new JPanel (new GridLayout (3, 2, 10, 10));
+        JPanel ouyputPanel = new JPanel(new FlowLayout (FlowLayout.CENTER, 10, 10));
 
+        inputPanel.add(new JLabel("Medicine NAme:"));
+        inputMedicine = new JTextField(15);
+        input Panel.add(inputMedicine);
 
+        inputPanel.add(new JLabel("Quantity:"));
+        inputQuantity = new JTextField(15);
+        inputPanel.add(inputQuantity);
+
+        JButton billButton =new JButton("Add to Bill");
+        billButom.addActionListener(this::addToBill);
+        inputPanel.add(billButton);
+
+        outputPanel.add(new JLabel("Total Price:"));
+        billTextField = new JTextField("0.0 BDT",  20);
+        billTextField.setEditable(false);
+        outputPanel.add(billTextField);
+
+        setLayout(new BorderLayout (10, 10));
+        add(inputPanel, BorderLayout.CENTER);
+        add(outputPanel, BorderLayout.SOUTH);
+    }
+
+    private void addToBill (ActionEvent e) {
 
     }
-    
 }
